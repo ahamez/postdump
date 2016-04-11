f47adfe06e2b54d09cf396dc4a0783ea5711fd59
Alexandre Hamez <alexandre.hamez+github@gmail.com>
2016-04-11 12:08:49 +0200

    Write posts to file and limit number of entries in memory

--- lib/postdump/posts.ex
+++ lib/postdump/posts.ex
@@ -2,17 +2,36 @@ defmodule Postdump.Posts do
 
 
   def start_link do
-    Agent.start_link(fn -> [] end, name: __MODULE__)
+    Agent.start_link(
+      fn ->
+        file = File.open! "posts.txt", [:write]
+        {[], file}
+      end,
+      name: __MODULE__
+    )
   end
 
 
   def get do
-    Agent.get(__MODULE__, &(&1))
+    Agent.get(__MODULE__,
+      fn {posts, _file} ->
+        posts
+      end
+    )
   end
 
 
-  def add(post) do
-    Agent.update(__MODULE__, &([post | &1]))
+  def add({headers, params}) do
+    Agent.update(__MODULE__,
+      fn {posts, file} ->
+        file
+        |> IO.binwrite(params["_json"] |> Poison.encode!)
+        file
+        |> IO.binwrite("\n")
+        new_posts = [{headers, params} | posts] |> Enum.take(100)
+        {new_posts, file}
+      end
+    )
   end
 
 
